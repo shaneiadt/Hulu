705d062685caa9a34d088516826ccd3dff96f009
Shane O'Moore <shaneiadt@gmail.com>
2022-03-11 12:03:56 +0000

    header component init

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='flex'>
+            <Image
+                className='object-contain'
+                src='https://links.papareact.com/ua6'
+                width={200}
+                height={100}
+            />
+        </header>
+    )
+}
+
+export default Header
