8017976d7be1a2aef19c9bd853c5c8441cf95313
Shane O'Moore <shaneiadt@gmail.com>
2022-03-12 12:17:08 +0000

    icon component items

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,18 @@
 import Image from 'next/image'
 import React from 'react'
+import HeaderItem from './HeaderItem'
+
+import { HomeIcon, LightningBoltIcon } from '@heroicons/react/outline';
 
 const Header = () => {
     return (
-        <header className='flex'>
+        <header>
+            <div>
+                <HeaderItem title='HOME' Icon={HomeIcon} />
+                <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
+            </div>
             <Image
-                className='object-contain'
+                className='object-contain flex-1'
                 src='https://links.papareact.com/ua6'
                 width={200}
                 height={100}
