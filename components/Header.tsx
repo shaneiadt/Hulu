dd2efd8e5eb6a7e55853b5db3e08b38d8e1b9490
Shane O'Moore <shaneiadt@gmail.com>
2022-03-14 13:50:13 +0000

    auto height

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ import { BadgeCheckIcon, CollectionIcon, HomeIcon, LightningBoltIcon, SearchIcon
 
 const Header = () => {
     return (
-        <header className='flex flex-col sm:flex-row m-5 justify-between items-center'>
+        <header className='flex flex-col sm:flex-row m-5 justify-between items-center h-auto'>
             <div className='flex flex-grow justify-evenly max-w-2xl'>
                 <HeaderItem title='HOME' Icon={HomeIcon} />
                 <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
