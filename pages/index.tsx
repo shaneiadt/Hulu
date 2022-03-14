9bd78fb9f2b080f85339e14d502c0d807db96705
Shane O'Moore <shaneiadt@gmail.com>
2022-03-14 14:37:56 +0000

    render nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Nav from '../components/Nav'
 
 const Home: NextPage = () => {
   return (
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
 
       <Header />
 
+      <Nav />
     </>
   )
 }
