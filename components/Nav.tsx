659873554f76b666c9e4304c3b53863b35ee70db
Shane O'Moore <shaneiadt@gmail.com>
2022-03-15 13:44:09 +0000

    useRouter to append query string

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,12 +1,17 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 import requests from '../utils/requests'
 
 const Nav = () => {
+    const router = useRouter();
+
     return (
         <nav className='relative'>
             <div className='scrollbar-hide flex whitespace-nowrap space-x-10 px-10 sm:px-20 text-2xl sm:space-x-20 overflow-x-scroll'>
                 {Object.entries(requests).map(([key, { title, url }]) => (
-                    <h2 className='last:pr-24 cursor-pointer transition duration-100 hover:scale-125 hover:text-white active:text-red-500' key={key}>{title}</h2>
+                    <h2
+                        onClick={() => router.push(`/?genre=${key}`)}
+                        className='last:pr-24 cursor-pointer transition duration-100 hover:scale-125 hover:text-white active:text-red-500' key={key}>{title}</h2>
                 ))}
             </div>
             <div className='absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12' />
