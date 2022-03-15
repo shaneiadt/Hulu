c840c7a1de67976d7bc4ba68b2ea0ff6f293293a
Shane O'Moore <shaneiadt@gmail.com>
2022-03-15 13:42:49 +0000

    scroll nav gradient

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,12 +3,14 @@ import requests from '../utils/requests'
 
 const Nav = () => {
     return (
-        <nav>
+        <nav className='relative'>
             <div className='scrollbar-hide flex whitespace-nowrap space-x-10 px-10 sm:px-20 text-2xl sm:space-x-20 overflow-x-scroll'>
                 {Object.entries(requests).map(([key, { title, url }]) => (
                     <h2 className='last:pr-24 cursor-pointer transition duration-100 hover:scale-125 hover:text-white active:text-red-500' key={key}>{title}</h2>
                 ))}
             </div>
+            <div className='absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12' />
+            <div className='absolute top-0 left-0 bg-gradient-to-r from-[#06202A] h-10 w-1/12' />
         </nav>
     )
 }
