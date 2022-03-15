19edd00936a07f657668cc3412cbf4ac21ed408b
Shane O'Moore <shaneiadt@gmail.com>
2022-03-15 14:48:43 +0000

    show movie poster image

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,9 +1,19 @@
+import Image from 'next/image';
 import React from 'react'
 import { Result } from '../utils/requests';
 
-const Thumbnail: React.FC<{ result: Result }> = () => {
+const BASE_IMG_URL = 'https://image.tmdb.org/t/p/original/';
+
+const Thumbnail: React.FC<{ result: Result }> = ({ result: { title, poster_path, backdrop_path } }) => {
     return (
-        <div>Thumbnail</div>
+        <div>
+            <Image
+                layout='responsive'
+                src={`${BASE_IMG_URL}${backdrop_path || poster_path}`}
+                height={1080}
+                width={1920}
+            />
+        </div>
     )
 }
 
