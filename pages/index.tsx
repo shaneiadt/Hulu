8197de1b177bc89c2a694f896a00b07150e31b56
Shane O'Moore <shaneiadt@gmail.com>
2022-03-15 14:42:43 +0000

    fetch results from TMDB

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
+import Results from '../components/Results'
+import requests, { Result } from '../utils/requests'
 
-const Home: NextPage = () => {
+interface Props {
+  results: Result[];
+}
+
+const Home: NextPage<Props> = ({ results }) => {
   return (
     <>
       <Head>
@@ -12,10 +18,23 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
-
       <Nav />
+      <Results results={results} />
     </>
   )
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const genre = query.genre;
+  const url = typeof genre === 'string' ? requests[genre].url : requests.fetechTrending.url;
+
+  const request: { results: Result[] } = await fetch(`https://api.themoviedb.org/3${url}`).then(res => res.json());
+
+  return {
+    props: {
+      results: request.results
+    }
+  };
+}
