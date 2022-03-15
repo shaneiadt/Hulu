6267bfc02fa5e469ada9cb6bf432f312d811cd60
Shane O'Moore <shaneiadt@gmail.com>
2022-03-15 15:10:52 +0000

    style result thumbnails

--- components/Results.tsx
+++ components/Results.tsx
@@ -8,7 +8,7 @@ interface IResultsProps {
 
 const Results: React.FC<IResultsProps> = ({ results }) => {
     return (
-        <div>
+        <div className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center'>
             {results.map(result => (
                 <Thumbnail key={result.id} result={result} />
             ))}
