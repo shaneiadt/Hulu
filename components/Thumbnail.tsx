62ef62bc73ed830c3a6d0f8e829bc930ad22f6c0
Shane O'Moore <shaneiadt@gmail.com>
2022-03-15 14:42:32 +0000

    thumbnail component

---
+++ components/Thumbnail.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Result } from '../utils/requests';
+
+const Thumbnail: React.FC<{ result: Result }> = () => {
+    return (
+        <div>Thumbnail</div>
+    )
+}
+
+export default Thumbnail;
